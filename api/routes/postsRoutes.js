f5e39dc6cc1d4332c253fea854ba83d3a2f8a5f8
Emmanuel Béziat <contact@emmanuelbeziat.com>
2018-04-10 03:03:01 +0200

    Update postsRoutes.js

--- api/routes/postsRoutes.js
+++ api/routes/postsRoutes.js
@@ -3,6 +3,7 @@
 module.exports = function (app) {
 	var articles = require('../controllers/articlesController.js')
 	var portfolio = require('../controllers/portfolioController.js')
+	var projects = require('../controllers/projectsController.js')
 
 	// Home
 	app.get('/', function (req, res) {
@@ -15,6 +16,9 @@ module.exports = function (app) {
 
 	app.route('/portfolio').get(portfolio.listAll)
 	app.route('/portfolio/:slug').get(portfolio.getSingle)
+	
+	app.route('/projects').get(projects.listAll)
+	app.route('/projects/:slug').get(projects.getSingle)
 
 	app.get('*', function (request, response) {
 		response.send('There is no valid data here', 404)
