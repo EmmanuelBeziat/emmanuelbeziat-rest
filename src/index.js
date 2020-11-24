cfa430d783ac63e4701f452c23c60df145e5ff49
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-11-24 06:37:44 +0100

    CORS test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import App from './App.js'
+import CORS from 'fastify-cors'
+
+App.register(CORS, {
+	origin: (origin, cb) => {
+		if (/localhost/.test(origin) || 'https://www.emmanuelbeziat.com') {
+			cb(null, true)
+			return
+		}
+
+		cb(new Error('Not allowed'))
+	}
+})
 
 App.listen(process.env.PORT || 3000, (err, address) => {
 	if (err) {
