4676006cda0372edb05a55f246cbc37eaf714cf9
Emmanuel Béziat <social@emmanuelbeziat.com>
2024-10-26 02:54:31 +0200

    fix: :bug: Fixed jest + slug

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,3 +1,11 @@
 module.exports = {
   setupFiles: ['<rootDir>/tests/jest.setup.js'],
+	transform: {
+    '^.+\\.jsx?$': 'babel-jest', // Use Babel to transform JS files
+  },
+  extensionsToTreatAsEsm: ['.jsx'], // Only treat .jsx files as ESM
+  transformIgnorePatterns: [
+    '/node_modules/(?!slug)', // Allow transformation of the slug package
+  ],
+  testEnvironment: 'node', // Set the test environment to Node
 }
