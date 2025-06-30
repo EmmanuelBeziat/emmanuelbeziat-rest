d1257dd224891037041c51bcda1e20fb0c3b9e06
Emmanuel Béziat <social@emmanuelbeziat.com>
2025-06-30 18:48:51 +0200

    ci: :wastebasket:

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,31 @@
-import js from "@eslint/js"
-import globals from "globals"
-import { defineConfig } from "eslint/config"
+import js from '@eslint/js'
+import globals from 'globals'
 
-export default defineConfig([
-  {
-		files: ["**/*.{js,mjs,cjs}"],
-		plugins: { js },
-		extends: ["js/recommended"],
-		rules: {
-			'no-tabs': 'off',
-			'brace-style': [
-				'error',
-				'stroustrup'
-			],
-			'comma-dangle': [
-				'error',
-				'only-multiline'
-			],
-			'no-unused-vars': [
-				'error', {
-					'argsIgnorePattern': '^_'
-				}
-			]
-		}
-	},
-  {
-		files: ["**/*.{js,mjs,cjs}"],
+export default [
+	// Appliquer les configurations recommandées par ESLint/JS comme base
+	js.configs.recommended,
+
+	// Ajouter notre configuration personnalisée par-dessus
+	{
+		files: ['**/*.{js,mjs,cjs}'],
 		languageOptions: {
 			globals: {
-				...globals.node
-			}
-		}
-	}
-])
+				...globals.node,
+			},
+		},
+		rules: {
+			// Règles provenant de l'ancien .eslintrc.js
+			'indent': ['error', 'tab'],
+			'linebreak-style': ['error', 'unix'],
+			'quotes': ['error', 'single'],
+			'semi': ['error', 'never'],
+			'no-console': 'off',
+
+			// Règles provenant de l'ancien eslint.config.js
+			'no-tabs': 'off',
+			'brace-style': ['error', 'stroustrup'],
+			'comma-dangle': ['error', 'only-multiline'],
+			'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+		},
+	},
+]
